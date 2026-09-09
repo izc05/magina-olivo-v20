0edf199619e2ceee49a173e128a5e5874f9771b9
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-09 22:45:08 +0200

    db: backfill OCR idempotency keys safely

--- database/migrations/0009_ocr_idempotency.sql
+++ database/migrations/0009_ocr_idempotency.sql
@@ -10,6 +10,10 @@ FROM document_versions AS dv
 JOIN documents AS d ON d.id = dv.document_id
 WHERE dv.id = o.document_version_id;
 
+UPDATE ocr_runs
+SET client_operation_id = id
+WHERE client_operation_id IS NULL;
+
 ALTER TABLE ocr_runs
   ALTER COLUMN workspace_id SET NOT NULL,
   ALTER COLUMN client_operation_id SET NOT NULL;
