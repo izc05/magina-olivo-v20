d3bbd0f8a7f5f4fa0cab4438c5e17c21283eb582
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-10 10:21:53 +0200

    feat(weather): export AEMET reflectivity palette resolver

--- packages/weather/src/index.ts
+++ packages/weather/src/index.ts
@@ -1,5 +1,6 @@
 export * from './aemet.js';
 export * from './radar.js';
 export * from './aemet-radar-geotiff.js';
+export * from './aemet-national-reflectivity-palette.js';
 export * from './radar-escala.js';
 export * from './radar-geotiff-inspection.js';
