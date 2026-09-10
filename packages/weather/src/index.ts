a402a4b8d447338c01fe9d9287ebe6d110f9e63c
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-10 07:54:58 +0200

    weather: export AEMET radar scale parser

--- packages/weather/src/index.ts
+++ packages/weather/src/index.ts
@@ -1,4 +1,5 @@
 export * from './aemet.js';
 export * from './radar.js';
 export * from './aemet-radar-geotiff.js';
+export * from './radar-escala.js';
 export * from './radar-geotiff-inspection.js';
