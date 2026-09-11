78358f113f08eab64c0ce9629bebd72a0edf0939
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-11 09:10:23 +0200

    Add professional invoice page

---
+++ apps/web/src/app/mi-campo/profesional/facturas/nueva/page.tsx
@@ -0,0 +1,5 @@
+import { ProfessionalInvoiceEntryClient } from '@/components/professional-invoice-entry-client';
+
+export default function ProfessionalInvoicePage() {
+  return <ProfessionalInvoiceEntryClient />;
+}
