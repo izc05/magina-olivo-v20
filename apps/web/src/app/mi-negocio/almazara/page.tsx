7a2ebb39b4e66299f020223a4f195b839fe6c588
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-14 22:38:30 +0200

    feat(web): surface reward unlock management in almazara panel

--- apps/web/src/app/mi-negocio/almazara/page.tsx
+++ apps/web/src/app/mi-negocio/almazara/page.tsx
@@ -1,5 +1,9 @@
 import { AlmazaraBusinessPanel } from '@/components/almazara-business-panel';
+import { AlmazaraRewardUnlockManager } from '@/components/almazara-reward-unlock-manager';
 
 export default function AlmazaraBusinessPage() {
-  return <AlmazaraBusinessPanel />;
+  return <>
+    <AlmazaraBusinessPanel />
+    <AlmazaraRewardUnlockManager />
+  </>;
 }
