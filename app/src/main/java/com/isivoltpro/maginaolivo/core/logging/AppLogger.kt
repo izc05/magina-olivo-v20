b9e3381cb4904ec6b5ebf94905f657b9e061142f
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-18 09:39:02 +0200

    feat(arch): add AppLogger.kt

---
+++ app/src/main/java/com/isivoltpro/maginaolivo/core/logging/AppLogger.kt
@@ -0,0 +1,8 @@
+package com.isivoltpro.maginaolivo.core.logging
+
+interface AppLogger {
+    fun debug(tag: String, message: String)
+    fun info(tag: String, message: String)
+    fun warning(tag: String, message: String, throwable: Throwable? = null)
+    fun error(tag: String, message: String, throwable: Throwable? = null)
+}
