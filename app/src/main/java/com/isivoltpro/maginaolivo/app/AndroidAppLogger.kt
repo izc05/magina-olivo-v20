92f9af26e5b72504afaf59ff421dbe26490ddcb7
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-18 09:39:04 +0200

    feat(arch): add AndroidAppLogger.kt

---
+++ app/src/main/java/com/isivoltpro/maginaolivo/app/AndroidAppLogger.kt
@@ -0,0 +1,22 @@
+package com.isivoltpro.maginaolivo.app
+
+import android.util.Log
+import com.isivoltpro.maginaolivo.core.logging.AppLogger
+
+class AndroidAppLogger : AppLogger {
+    override fun debug(tag: String, message: String) {
+        Log.d(tag, message)
+    }
+
+    override fun info(tag: String, message: String) {
+        Log.i(tag, message)
+    }
+
+    override fun warning(tag: String, message: String, throwable: Throwable?) {
+        Log.w(tag, message, throwable)
+    }
+
+    override fun error(tag: String, message: String, throwable: Throwable?) {
+        Log.e(tag, message, throwable)
+    }
+}
