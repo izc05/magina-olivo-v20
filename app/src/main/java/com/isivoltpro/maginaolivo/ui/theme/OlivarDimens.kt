9a6e4a66a1f2825739136a42ed7ce73b2f34ecba
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-18 10:06:39 +0200

    feat(ui): carry OlivarDimens.kt

---
+++ app/src/main/java/com/isivoltpro/maginaolivo/ui/theme/OlivarDimens.kt
@@ -0,0 +1,22 @@
+package com.isivoltpro.maginaolivo.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object OlivarDimens {
+    val Space2Xs = 4.dp
+    val SpaceXs = 8.dp
+    val SpaceSm = 12.dp
+    val SpaceMd = 16.dp
+    val SpaceLg = 24.dp
+    val SpaceXl = 32.dp
+    val Space2Xl = 48.dp
+
+    val RadiusSm = 10.dp
+    val RadiusMd = 14.dp
+    val RadiusLg = 18.dp
+    val RadiusXl = 24.dp
+
+    val TouchTarget = 48.dp
+    val PrimaryButtonHeight = 52.dp
+    val ScreenPadding = 16.dp
+}
