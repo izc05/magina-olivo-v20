96d4095960205cc69e184dde777108d67dcaf813
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-18 23:01:24 +0200

    fix(ui): retain Color type for map legend

--- app/src/main/java/com/isivoltpro/maginaolivo/ui/reference/map/MapCatastroReferenceScreen.kt
+++ app/src/main/java/com/isivoltpro/maginaolivo/ui/reference/map/MapCatastroReferenceScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.testTag
