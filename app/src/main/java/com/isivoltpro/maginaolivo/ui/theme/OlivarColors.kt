aefd7222bf98a7988bb6779467abb236932e9733
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-18 12:36:23 +0200

    feat(ui): carry OlivarColors.kt

---
+++ app/src/main/java/com/isivoltpro/maginaolivo/ui/theme/OlivarColors.kt
@@ -0,0 +1,34 @@
+package com.isivoltpro.maginaolivo.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object OlivarColors {
+    val Olive900 = Color(0xFF2F3A1C)
+    val Olive800 = Color(0xFF3F4D25)
+    val Olive700 = Color(0xFF556B2F)
+    val Olive600 = Color(0xFF6D803E)
+    val Olive500 = Color(0xFF7F944B)
+
+    val Sage600 = Color(0xFF78935F)
+    val Sage500 = Color(0xFF8DA874)
+    val Sage200 = Color(0xFFDDE7D3)
+
+    val Cream50 = Color(0xFFFFFDF8)
+    val Cream100 = Color(0xFFF8F4E9)
+    val Cream200 = Color(0xFFF1EAD8)
+
+    val Earth500 = Color(0xFFC39A68)
+    val Earth600 = Color(0xFF95693F)
+
+    val Charcoal900 = Color(0xFF20231D)
+    val Charcoal700 = Color(0xFF4C5148)
+    val Charcoal500 = Color(0xFF71766C)
+
+    val Line200 = Color(0xFFE1E5DA)
+    val White = Color(0xFFFFFFFF)
+
+    val Success = Color(0xFF3F7D43)
+    val Warning = Color(0xFFA3650E)
+    val Error = Color(0xFFB6463A)
+    val Info = Color(0xFF47789A)
+}
