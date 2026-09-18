a2ab3d16474c5888fc8ef045be37464f43f07a50
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-18 10:03:12 +0200

    fix(ui): correct imports in MoFields.kt

--- app/src/main/java/com/isivoltpro/maginaolivo/ui/components/MoFields.kt
+++ app/src/main/java/com/isivoltpro/maginaolivo/ui/components/MoFields.kt
@@ -6,7 +6,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardOptions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 
 @Composable
