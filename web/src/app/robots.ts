21c23f9310014b7ac9dbdd8ea2f9c0bd2a459b03
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-20 15:56:24 +0200

    web: block staging indexing by default

--- web/src/app/robots.ts
+++ web/src/app/robots.ts
@@ -1,11 +1,17 @@
 import type { MetadataRoute } from "next";
+import { ALLOW_INDEXING, SITE_URL } from "@/lib/site";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: "https://maginaolivo.es/sitemap.xml",
+    rules: ALLOW_INDEXING
+      ? {
+          userAgent: "*",
+          allow: "/",
+        }
+      : {
+          userAgent: "*",
+          disallow: "/",
+        },
+    sitemap: `${SITE_URL}/sitemap.xml`,
   };
 }
