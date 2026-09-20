af125b9ae4706947ebab18209cf36f1e211f2049
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-20 22:31:23 +0200

    fix(web): make robots.ts static-export compatible

--- web/src/app/robots.ts
+++ web/src/app/robots.ts
@@ -1,6 +1,9 @@
 import type { MetadataRoute } from "next";
 import { ALLOW_INDEXING, SITE_URL } from "@/lib/site";
 
+export const dynamic = "force-static";
+
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: ALLOW_INDEXING
