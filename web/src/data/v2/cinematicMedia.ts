2f5c14bab2c2bd41c7c016a4e090458353188cef
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-09-21 14:23:07 +0200

    asset(web-v2): register 4K olive-field film candidate

---
+++ web/src/data/v2/cinematicMedia.ts
@@ -0,0 +1,12 @@
+export const v2CinematicFieldFilm = {
+  id: "olive-pruning-field-film",
+  video:
+    "https://videos.pexels.com/video-files/20606525/20606525-uhd_3840_2160_24fps.mp4",
+  poster:
+    "https://images.pexels.com/videos/20606525/pexels-photo-20606525.jpeg?auto=compress&cs=tinysrgb&w=2200",
+  sourcePage:
+    "https://www.pexels.com/video/a-man-is-standing-in-front-of-an-olive-tree-20606525/",
+  resolution: "3840x2160",
+  fps: 24,
+  license: "Pexels Free",
+} as const;
